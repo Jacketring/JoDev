109f3ecd8227be5fb6248aaaf733faf921058024
Jose Hurtado <josehur2003@gmail.com>
2026-04-10 11:20:07 +0200

    Fix React Vite refresh preamble

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
         <title>{{ config('app.name', 'JoDev CRM') }}</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=manrope:400,500,600,700,800|outfit:500,600,700" rel="stylesheet" />
+        @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     </head>
     <body>
